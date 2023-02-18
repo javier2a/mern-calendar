bbaa150f5f2034e8877dfb525c22f5ccded3ec80
jav_dev <anayaacostajavier@gmail.com>
2023-02-18 17:37:08 -0500

    path * agregado nuevamente

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path')
 
 // Crear el servidor de express
 const app = express();
@@ -13,7 +14,8 @@ dbConnection();
 app.use(cors())
 
 // Directorio Público
-app.use( express.static('public') );
+// app.use( express.static('public') );
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Lectura y parseo del body
 app.use( express.json() );
@@ -22,10 +24,12 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
-app.get('*', ( req, res ) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
-
+// app.get('*', ( req, res ) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// })
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 
